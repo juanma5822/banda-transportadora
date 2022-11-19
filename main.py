00bd9bbd7f99406ed98884f92e9728192f22f044
Juan Romero <juan.romero5822@gmail.com>
2022-11-19 09:44:15 -0500

    Final_process

--- main.py
+++ main.py
@@ -1,129 +1,66 @@
+import pandas as pd
 from flask import Flask, redirect, url_for, request, json, render_template
 import psycopg2
-import RPi.GPIO as GPIO
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 from time import sleep
-import time 
+from matplotlib import cm
+from matplotlib import colors
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-#Entradas
-sensor1 = 23
-sensor2 = 24
-GPIO.setup(sensor1, GPIO.IN)
-GPIO.setup(sensor2, GPIO.IN)
-#Salidas 
-motor_banda = 18
-servo1 = 17
-servo2 = 27
-motor_caja_grande1 = 5
-motor_caja_grande2 = 6
-motor_caja_pequena1 = 13
-motor_caja_pequena2= 19
-GPIO.setup(motor_banda, GPIO.OUT)
-GPIO.setup(motor_caja_grande1, GPIO.OUT)
-GPIO.setup(motor_caja_grande2, GPIO.OUT)
-GPIO.setup(motor_caja_pequena1, GPIO.OUT)
-GPIO.setup(motor_caja_pequena2, GPIO.OUT)
-GPIO.setup(servo1, GPIO.OUT)
-GPIO.setup(servo2, GPIO.OUT)
-#Configuracion de alertar y modo
-
-
-serv1 = GPIO.PWM(servo1, 50) # GPIO 17 for PWM with 50Hz
-serv2 = GPIO.PWM(servo2, 50) # GPIO 17 for PWM with 50Hz
-try:
-#conexion base de datos
- conexion=psycopg2.connect(user='xxbafeigeplqzf',
+app = Flask(__name__)
+conexion=psycopg2.connect(user='xxbafeigeplqzf',
                           password='292103424cc1246aebe3a724ba93b366f7bcb06ae9d2c700b1cfd305b03e6d80',
                           host='ec2-34-227-120-79.compute-1.amazonaws.com',
                           port=5432,
                           database='d4reu6rdlo91uc')
 
-
- cursor=conexion.cursor()
-
- sql='INSERT INTO datosrecibidos (caja_grande,caja_pequeña) VALUES (%s,%s)'
- print('base conectada')
-except:
- print('no se conecto')
+cursor=conexion.cursor()
 
 
-caja_grande_contador = 0
-caja_pequena_contador = 0
+@app.route("/",methods=("POST","GET"))
+def index():
+    cursor
+    sql=("SELECT * from datosrecibidos ")
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    df = pd.DataFrame(data,columns=["id",'cajas_p','cajas_g'])
+    df['cajas_p']=df['cajas_p'].astype('int64')
+    df['cajas_g']=df['cajas_g'].astype('int64')
+    print(df.info())
+    suma_p=df['cajas_p'].sum()
+    suma_g=df['cajas_g'].sum()
+    datos=[suma_p,suma_g]
+    ejex=['cajas pequeñas','cajas grandes']
+    #fig, ax = plt.subplots(1,2)
+    normdata= colors.Normalize(0, max(datos))
+    colormap = cm.get_cmap("Blues")
+    colores =colormap(normdata(datos))
+    colores_list=['Red','Orange']
+    graph=plt.bar(ejex,datos,color=colores_list)
+    i = 0
+    for p in graph:
+        width = p.get_width()
+        height = p.get_height()
+        x, y = p.get_xy()
+        plt.text(x+width/2,
+                y+height*1.01,
+                str(datos[i]),
+                ha='center',
+                weight='bold')
+        i+=1
+    plt.savefig("static/Ejemplo3.jpg")
+    plt.close()
+    desfase = (0.2, 0.1)
+    plt.pie(datos,labels=ejex,autopct="%0.1f %%",colors=colores,explode=desfase)
+    plt.savefig("static/Ejemplo4.jpg")
+    plt.close()
+    return render_template('index.html')    
 
+@app.route("/contacto",methods=("POST","GET"))
+def contactos():
+    return render_template('contact.html')
 
-
-while(True):   
-        
-    caja_grande = 0
-    caja_pequena = 0
-    # Se inicia Banda Transportadora
-    GPIO.output(motor_banda,GPIO.HIGH)
-    #Cajas Grandes
-    if GPIO.input(sensor1) and GPIO.input(sensor2):
-        serv2.ChangeDutyCycle(7.5)
-        serv2.stop()
-        serv1.start(7.5)
-        serv1.ChangeDutyCycle(12.5)
-        sleep(5)
-        serv1.ChangeDutyCycle(7.5)
-        serv1.stop()
-        caja_grande = 1
-        caja_grande_contador +=1
-    #Cajas pequeñas    
-    elif  GPIO.input(sensor1) and GPIO.input(sensor2) ==False:
-        serv1.ChangeDutyCycle(7.5)
-        serv1.stop()
-        serv2.start(7.5)
-        serv2.ChangeDutyCycle(12.5)
-        sleep(5)
-        serv2.ChangeDutyCycle(7.5)
-        serv2.stop()
-        caja_pequena = 1
-        caja_pequena_contador +=1
-    else:
-        serv1.start(7.5)
-        serv2.start(7.5)
-        serv1.stop()
-        serv2.stop()
-
-    #Contar cajas grandes y comprimirlas
-    if caja_grande_contador == 5:
-        GPIO.output(motor_caja_grande1,GPIO.HIGH)
-        GPIO.output(motor_caja_grande2,GPIO.LOW)
-        sleep(5)
-        GPIO.output(motor_caja_grande1,GPIO.LOW)
-        GPIO.output(motor_caja_grande2,GPIO.HIGH)
-        sleep(5)
-        caja_grande_contador = 0
-    #Contar cajas pequenas y comprimirlas    
-    elif caja_pequena_contador == 5:
-        GPIO.output(motor_caja_pequena1,GPIO.HIGH)
-        GPIO.output(motor_caja_pequena2,GPIO.LOW)
-        sleep(5)
-        GPIO.output(motor_caja_pequena1,GPIO.LOW)
-        GPIO.output(motor_caja_pequena2,GPIO.HIGH)
-        sleep(5)
-        caja_pequena_contador = 0
-    else:
-        GPIO.output(motor_caja_pequena1,GPIO.LOW)
-        GPIO.output(motor_caja_pequena2,GPIO.LOW)
-        GPIO.output(motor_caja_grande1,GPIO.LOW)
-        GPIO.output(motor_caja_grande2,GPIO.LOW)
-
-
-    datos=(caja_grande,caja_pequena)
-
-    cursor.execute(sql,datos)
-
-    #guardar registro
-    conexion.commit()
-
-    #registro insertado
-    registros=cursor.rowcount
-
-    #mostra mensaje
-    print(f'registro insertado: {registros}')
-    print(f'El contador de cajas grandes esta en {caja_grande_contador}')
-    print(f'El contadot de cajas pequeñas esta en {caja_pequena_contador}')
-
+if __name__ == '__main__':
+    app.run(port=5432)
+    app.run(debug=True)
